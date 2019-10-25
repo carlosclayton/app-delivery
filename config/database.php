8444d967c9ae447e1ca28a7ae319a7ba1666cb91
Carlos Clayton <carlos.clayton@gmail.com>
2019-10-25 19:55:22 -0300

    Updating database connections

--- config/database.php
+++ config/database.php
@@ -2,6 +2,9 @@
 
 use Illuminate\Support\Str;
 
+
+$databaseUrl = parse_url(env("DATABASE_URL"));
+
 return [
 
     /*
@@ -65,18 +68,18 @@ return [
 
         'pgsql' => [
             'driver' => 'pgsql',
-            'url' => env('DATABASE_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'host' => env('DB_HOST', $databaseUrl['host']),
+            'port' => env('DB_PORT', $databaseUrl['port']),
+            'database' => env('DB_DATABASE', substr($databaseUrl['path'], 1)),
+            'username' => env('DB_USERNAME', $databaseUrl['user']),
+            'password' => env('DB_PASSWORD', $databaseUrl['password']),
             'charset' => 'utf8',
             'prefix' => '',
             'prefix_indexes' => true,
             'schema' => 'public',
             'sslmode' => 'prefer',
         ],
+
 
         'sqlsrv' => [
             'driver' => 'sqlsrv',
