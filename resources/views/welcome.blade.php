4b7ed6cdde205f0128e0c42f4aac775b58591aa9
Carlos Clayton <carlos.clayton@gmail.com>
2019-10-25 22:34:33 -0300

    Updating app.json

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel 5.8.12
+                    Laravel 5.8.13
                 </div>
 
                 <div class="links">
