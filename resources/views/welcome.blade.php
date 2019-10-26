26a9ca05b7288a354ece514a63b9777045ab65ec
Carlos Clayton <carlos.clayton@gmail.com>
2019-10-25 21:04:51 -0300

    Updating Wellcome view to 5.8.8

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel 5.8.7
+                    Laravel 5.8.8
                 </div>
 
                 <div class="links">
